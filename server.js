8dc038768fb90874110598073482c42b9d988d01
Thomas <13186147+period@users.noreply.github.com>
2019-12-03 11:45:03 +0000

    Simple server base

--- server.js
+++ server.js
@@ -22,13 +22,30 @@ app.get("/create-game", function(req, res) {
 io.on("connection", function(socket) {
     socket.on("join-room", function(data) {
         data = data.toString();
-        if(games[data] == null) return socket.emit("eval", "window.location.href = \"./\"");
+        //if(games[data] == null) return socket.emit("eval", "window.location.href = \"./\"");
+        socket.gid = data;
         socket.join(data);
         socket.emit("message", {type: "success", message: "Successfully joined game"});
-
+    })
+    socket.on("roll-dice", function() {
+        rollDice(socket.gid);
     })
 });
 
 http.listen(1337, function() {
     console.log("WSS server started on 1337");
 });
+
+function rollDice(gameId, callback) {
+    var counter = 10;
+    var diceRoller = setInterval(() => {
+        var diceA = Math.floor(Math.random() * 6) + 1;
+        var diceB = Math.floor(Math.random() * 6) + 1;
+        io.to(gameId).emit("dice", {a: diceA, b: diceB});
+        counter--;
+        if(counter == 0) {
+            clearInterval(diceRoller);
+            callback(diceA, diceB);
+        }
+    }, 100);
+}
